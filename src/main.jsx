743c9e75bef42a629dfc4ef3a23ae87636d22d84
Mathias Kristiansen <matkri3110@gmail.com>
2025-03-14 19:52:38 +0100

    Laget kommentarer for main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+// main.jsx er hovedfilen i prosjektet. Denne filen er ansvarlig for å rendre App komponenten i DOMen.
 import { createRoot } from 'react-dom/client'
 import './index.css'
+// Importerer App komponenten.
 import App from './App.jsx'
+// Importerer BrowserRouter fra react-router-dom.
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
+  // Har byttet ut StrictMode med BrowserRouter for å kunne bruke react-router-dom.
   <BrowserRouter>
     <App />
   </BrowserRouter>,
