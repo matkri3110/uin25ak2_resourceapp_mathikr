08763d11c8609ae884e1384c76f27769c2d937f3
Mathias Kristiansen <matkri3110@gmail.com>
2025-03-14 19:50:16 +0100

    Laget kommentarer for App.jsx + fjernet unødvendige imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-
+// App.jsx er hovedkomponenten i prosjektet. 
+// Denne komponenten er en funksjonell komponent som returnerer en Layout komponent som igjen inneholder en Routes komponent fra react-router-dom.
+// Importerer useState fra react.
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+// Importerer main.scss filen som inneholder de globale stilene for prosjektet.
 import './styles/scss/main.scss'
+// Importerer Layout komponenten.
 import Layout from './components/Layout'
+// Importerer Resources komponenten.
 import Resources from './components/Resources'
+// Importerer Routes og Route fra react-router-dom.
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    // Returnerer Layout komponenten som inneholder Routes komponenten.
     <Layout>
+      {/* Setter opp ruter for alle kategoriene som skal vises på siden. */ }
       <Routes>
-        <Route path="/html" element = { <Resources category="html" /> } />
-        <Route path="/css" element = { <Resources category="css" /> } />
-        <Route path="/javascript" element = { <Resources category="javascript" /> } />
-        <Route path="/react" element = { <Resources category="react" /> } />
-        <Route path="/headless-cms" element = { <Resources category="headless-cms" /> } />
+        <Route path="/html" element = { <Resources category="html" /> } /> {/* Setter opp rute for HTML */}
+        <Route path="/css" element = { <Resources category="css" /> } /> {/* Setter opp rute for CSS */}
+        <Route path="/javascript" element = { <Resources category="javascript" /> } /> {/* Setter opp rute for JavaScript */}
+        <Route path="/react" element = { <Resources category="react" /> } /> {/* Setter opp rute for React */}
+        <Route path="/headless-cms" element = { <Resources category="headless-cms" /> } /> {/* Setter opp rute for Sanity */}
       </Routes>
     </Layout>
   )
