903c40188e0d02c38929510b5a7c6d0329e0c22c
Mathias Kristiansen <matkri3110@gmail.com>
2025-03-14 19:11:03 +0100

    Laget kommentarer til PageTitle.jsx

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,5 +1,8 @@
+// Lager komponenten PageTitle som tar inn en prop "Hovedoverskrift" og returnerer en h1 tag med denne propen som innhold.
 export default function PageTitle({Hovedoverskrift}) {
     return (
+        // Skriver ut sidens hovedoversikt.
         <h1 id="Hovedoverskrift">{Hovedoverskrift}</h1>
     )
 }
+
