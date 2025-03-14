6aa979132683811e5a6400fddb281d3087cc6a13
Mathias Kristiansen <matkri3110@gmail.com>
2025-03-14 19:01:50 +0100

    Skrevet kommentarer for Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,19 @@
+// Navigansjonskomponentet på siden.
+
+// Importerer Link fra react-router-dom for å kunne lage linker til andre sider.
 import {Link} from 'react-router-dom';
+// Importerer den tilhørende stilfilen for navigasjonsmenyen.
 import './Nav.scss';
 
 export default function Nav() {
 
     
     return(
+        // Header seksjon som inneholder navigasjonsmenyen for siden. 
         <header>
             <nav>
                 <ul>
+                    {/* Lager en liste med li-tagger som alle inneholder en "Link to" komponent som sørger for at linkene navigerer til riktig sider. */}
                     <li><Link to="/html">HTML</Link></li>
                     <li><Link to="/css">CSS</Link></li>
                     <li><Link to="/javascript">JavaScript</Link></li>
